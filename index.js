9123e6007ba6e873ab792f4572db102d40e06f6b
An Nguyen <annguyez>
2024-01-07 12:24:54 +0700

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/hello', (req, res) => {
-    res.send('Hello World!  welcome to Jenkins')
+    res.send('Hello World!  welcome to Jenkins 2')
 })
 
 app.listen(port, () => {
